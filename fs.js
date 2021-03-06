edd8d8815ba766230fc13625d6b04263ef02bbb9
zhangheng05 <zhangheng05@baijiahulian.com>
2021-03-06 17:59:24 +0800

    feat(更新): 更新file模块

--- fs.js
+++ fs.js
@@ -2,4 +2,9 @@
 
 // 同步操作的方法写上sync， 异步操作的方法默认什么也不带
 
-//
+/**
+ * 常用 得方法有fs.mkdir 创建目录，fs.rmdir 删除目录， fs.writeFile 异步写文件， fs.readFile 异步读文件，
+ * fs.unlink 异步删除文件, fs.createReadStream(path) 获取读取文件的stream， fs.createWriteStream
+ * 获取写入文件的stream
+ *
+ */
